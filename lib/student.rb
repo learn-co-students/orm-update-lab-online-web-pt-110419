6250ef6891cd7172f108ec59dae5d709296f3e09
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-04 12:01:47 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -32,16 +32,17 @@ class Student
   def save
     sql_insert = "INSERT INTO students(name, grade) VALUES(?,?)"
     sql_select = "SELECT * FROM students"
-    sql_update = "UPDATE students SET name = #{self.name} WHERE id = #{self.id}"
+    sql_update = "UPDATE students SET name = '#{self.name}' WHERE id = '#{self.id}'"
     
     if self.id !=nil # If id not nil - we know object has already been persisted
       DB[:conn].execute(sql_update)
-    end
-    
-    if self.id == nil
+      puts "record updated"
+    elsif self.id == nil
        DB[:conn].execute(sql_insert, self.name, self.grade)
        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+       puts "new record saved"
     end
+    self
   end
       
   def self.create(name:, grade:)
@@ -61,6 +62,5 @@ class Student
   
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-
-
 end
+binding.pry
